ad7c05543c2ea2928c59a17e50d6e9c0bc00f42f
alfred.mayer@namk.com <alfred.mayer@namk.com>
2019-03-02 15:44:56 +0100

    Test

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -5,22 +5,17 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace MyWinFormsApp
+namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-            this.Text="fred";
-        }
-
-        private void buttonExit_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
+          System.Console.WriteLine( 123);
+          this.Text = "Fred";
         }
     }
 }
